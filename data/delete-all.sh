6405b96c1d25373d301acb6e50cffc2de9bb8867
Doug Harris <doug.harris@xsilosystems.com>
2021-05-28 09:31:24 -0400

    Retrieve password from keyvault

--- data/delete-all.sh
+++ data/delete-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-export CB_REST_USERNAME=$1
-export CB_REST_PASSWORD=$2
+module=$1
+
+export CB_REST_USERNAME=Administrator
+salt=$(azure/fetchsecret.sh ${module}Salt)
+key=$(echo -n $salt | hexdump -ve '1/1 "%.2x"')
+password_enc=$(azure/fetchsecret.sh ${module}gluuPW)
+CB_REST_PASSWORD=$(echo ${password_enc} | openssl enc -d -des-ede3 -K ${key} -nosalt -a)
+export CB_REST_PASSWORD
 
 /opt/couchbase/bin/couchbase-cli  bucket-delete -c localhost:8091 \
     --bucket gluu
