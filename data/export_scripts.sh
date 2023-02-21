241512b86252b6a83bedccf5abb07fb7a3ebab6b
Doug Harris <doug.harris@xsilosystems.com>
2023-02-21 12:19:07 -0500

    New tools to manage script configs

---
+++ data/export_scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+module=$1
+user=$2
+directory=$3
+
+salt=$(azure/fetchsecret.sh ${module}Salt)
+key=$(echo -n $salt | hexdump -ve '1/1 "%.2x"')
+password_enc=$(azure/fetchsecret.sh ${module}gluuPW)
+PASSWORD=$(echo ${password_enc} | openssl enc -d -des-ede3 -K ${key} -nosalt -a)
+export PASSWORD
+file=${directory}/scripts-$(date +"%F")
+
+/opt/couchbase/bin/cbq -u $user -p $PASSWORD -s "SELECT gluu.* FROM gluu WHERE oobjectClass='oxCustomScript' AND inum like 'AFSC-%'" |
+    sed -n '/^{/,$p' | jq '.results'> ${file}.json
+openssl enc -aes-256-cbc -pass env:PASSWORD -in ${file}.json -out ${file}.enc
